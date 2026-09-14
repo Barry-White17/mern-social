47c3cb258d934d7a12df25a78c732e117d4e2f26
Barry-White17 <whitwb2@gmail.com>
2026-09-14 15:29:07 +0100

    Update BACKEND_URL to production server

--- client/config.js
+++ client/config.js
@@ -1,4 +1,4 @@
 const config = {
-    BACKEND_URL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000',
+    BACKEND_URL: import.meta.env.VITE_BACKEND_URL || 'server-nu-teal.vercel.app',
 }
 export default config
