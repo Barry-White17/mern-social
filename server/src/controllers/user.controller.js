1170cb6eecd6f3bd3bad58c0f12fd0c377c5a0e6
Barry-White17 <whitwb2@gmail.com>
2026-09-15 19:41:29 +0100

    Fix JSON parsing in user creation logging

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -7,7 +7,7 @@ import { firstValues } from 'formidable/src/helpers/firstValues.js'
 import bcrypt from 'bcrypt'
 
 const create = async (req, res) => {
-    console.log(`Body: ${req.body}`)
+    console.log(`Body: ${JSON.parse(req.body)}`)
     console.log(`Content Type: ${req.headers['content-type']}`)
     const user = new User(req.body)
     try {
