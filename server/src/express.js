5f6dbcc1f3f0de60f19c48517234f925ac4cabe2
Barry-White17 <whitwb2@gmail.com>
2026-09-15 10:05:45 +0100

    Disable JSON and URL-encoded body parsing
    Comment out express.json and express.urlencoded middleware.

--- server/src/express.js
+++ server/src/express.js
@@ -24,8 +24,8 @@ const allowedOrigins = [process.env.FRONTEND_URL, process.env.FRONTEND_TEST_URL]
 
 // for vercel's serverless function stream handling
 
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+// app.use(express.json())
+// app.use(express.urlencoded({extended: true}))
 
 // cookieParser converts our cookies, which are by default strings into usable javaScript objects
 app.use(cookieParser())
