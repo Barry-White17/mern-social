109bde0cacad36edcd6a79fb1bbf0c05bb404a94
Barry-White17 <whitwb2@gmail.com>
2026-09-15 20:52:37 +0100

    Add logging for request body in user creation
    Log the request body when creating a user.

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -7,6 +7,7 @@ import { firstValues } from 'formidable/src/helpers/firstValues.js'
 import bcrypt from 'bcrypt'
 
 const create = async (req, res) => {
+    console.log(`Req body: ${req.body}`)
     const user = new User(req.body)
     try {
         await user.save()
