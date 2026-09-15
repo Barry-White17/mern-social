c85bc54afb4294fe66dfe0326a973131a4d6c9d1
Barry-White17 <whitwb2@gmail.com>
2026-09-15 10:18:53 +0100

    Update userController.create to display req.body

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -7,6 +7,8 @@ import { firstValues } from 'formidable/src/helpers/firstValues.js'
 import bcrypt from 'bcrypt'
 
 const create = async (req, res) => {
+    console.log(`Body: ${req.body}`)
+    console.log(`Content Type: ${req.headers['content-type']}`)
     const user = new User(req.body)
     try {
         await user.save()
