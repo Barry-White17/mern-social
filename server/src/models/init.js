fffb77af23e83e95edde5e3659320e64c977f559
Barry-White17 <whitwb2@gmail.com>
2026-09-23 22:15:29 +0100

    Add success message to database connection

--- server/src/models/init.js
+++ server/src/models/init.js
@@ -6,6 +6,7 @@ export function initDatabase() {
     const DATABASE_URL = process.env.DATABASE_URL
     mongoose.connection.on('open', () => {
         console.info('successfully connected to database:', DATABASE_URL)
+        console.log(`Successfully connected`)
     })
     const connection = mongoose.connect(DATABASE_URL)
     return connection
