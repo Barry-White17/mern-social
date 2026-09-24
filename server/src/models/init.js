730a33d09b1d5c51daac9d7ebea1168cc4dd56fd
Barry-White17 <whitwb2@gmail.com>
2026-09-24 09:08:44 +0100

    Refactor initDatabase and add connectionForServerlessEnvs
    Refactor database initialization to support serverless environments and caching.

--- server/src/models/init.js
+++ server/src/models/init.js
@@ -2,7 +2,7 @@
 // dotenv.config()
 import mongoose from 'mongoose'
 
-export function initDatabase() {
+cosnt initDatabase = () => {
     const DATABASE_URL = process.env.DATABASE_URL
     mongoose.connection.on('open', () => {
         console.info('successfully connected to database:', DATABASE_URL)
@@ -10,4 +10,24 @@ export function initDatabase() {
     })
     const connection = mongoose.connect(DATABASE_URL)
     return connection
+}
+if(!cached){
+    cached= global.mongoose= {
+        connection: null,
+        promise: null
+    }
+}
+const connectionForServerlessEnvs= async() => {
+    if(cached.conn){
+        return cached.conn
+    }
+    if(!cached.promise){
+        cached.promise= mongoose.connect(process.env.DATABASE_URL)
+    }
+    cached.conn= await cached.promise
+    return cached.conn
+}
+export {
+    initDatabase,
+    connectionForServerlessEnvs
 }
